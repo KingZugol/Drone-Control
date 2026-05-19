aa562c7cdd7a341a8229d3a816ca905ab067767d
Lars <test@test.com>
2026-05-19 14:16:33 +0200

    mavlink script to listen to channel 8 (SD Button)

---
+++ channel-listener.py
@@ -0,0 +1,15 @@
+from pymavlink import mavutil
+import time
+master = mavutil.mavlink_connection('/dev/serial0', baud=57600)
+master.wait_heartbeat()
+print("Connected to Ardupilot!")
+
+try:
+    while True:
+        message = master.recv_match(match='RC_CHANNELS', blocking=True)
+        pwm_val = message.chan9_raw
+        print(f"Rwm PWM recieved: {rawm_pwm}")
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("Exiting...")
+    
