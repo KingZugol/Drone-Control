8e5b25d10bd1272cd8716deb3b34460dc022d240
Lars <test@test.com>
2026-05-19 15:08:37 +0200

    Fixing wrong command

--- channel-listener.py
+++ channel-listener.py
@@ -6,18 +6,18 @@ print("Connected to Ardupilot!")
 
 message= master.mav.command_long_encode(
     master.target_system,
-    master.targe_component,
+    master.target_component,
     mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
     0,
-    65  # Command Id: RC_CHANNELS
+    65,  # Command Id: RC_CHANNELS
     50000,  # Interval in ms 50000 = 20hz
     0, 0, 0, 0, 0 # Empty params
 )
 master.mav.send(message)
-response = master.recv_match(type='COMMAND_ACK', blocking=True)
+response = master.recv_match(type='RC_CHANNELS', blocking=True)
 if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
     print("Command accepted")
-    pwm_val = response.chann8_raw
+    pwm_val = response.chan8_raw
     print("Num channels: " + response.chan_count)
     print(f"All message attributes: {response.__dir__()}")
     print(f"Raw PWM recieved: {pwm_val}")
