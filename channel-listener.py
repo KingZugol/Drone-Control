e82be62cedece8d4c1c9acbb3f46a70a63705aa6
Lars <test@test.com>
2026-05-19 15:18:27 +0200

    Fixed logic

--- channel-listener.py
+++ channel-listener.py
@@ -17,16 +17,16 @@ master.mav.send(message)
 response = master.recv_match(type='COMMAND_ACK', blocking=True)
 if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
     print("Command accepted")
+    try:
+        while True:
+            channel_response = master.recv_match(type='RC_CHANNELS', blocking=True)
+            print("Num channels: " + channel_response.chan_count)
+            pwm_val = channel_response.chan8_raw
+            print(f"Raw PWM recieved: {pwm_val}")
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Exiting...")
 else:
     print("Command failed")
 
-try:
-    while True:
-        channel_response = master.recv_match(type='RC_CHANNELS', blocking=True)
-        print("Num channels: " + channel_response.chan_count)
-        print(f"All message attributes: {channel_response.__dir__()}")
-        pwm_val = channel_response.chan8_raw
-        print(f"Raw PWM recieved: {pwm_val}")
-        time.sleep(0.5)
-except KeyboardInterrupt:
-    print("Exiting...")
+
