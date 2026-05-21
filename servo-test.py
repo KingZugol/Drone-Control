526b3f9fcc419bf371e336b5c37265286d368560
Lars <test@test.com>
2026-05-21 22:12:39 +0200

    servo

---
+++ servo-test.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+#GPIO numbering mode
+GPIO.setmode(GPIO.BOARD)
+#Pin 18 als output
+GPIO.setup(18,GPIO.OUT)
+servo1=GPIO.PWM(18,50) #Pin 18 out, 50hz
+
+servo1.start(0)
+print("Rotating servo 180 degrees")
+duty = 2
+
+while duty <= 12:
+    servo1.ChangeDutyCycle(duty)
+    time.sleep(1)
+    duty +=duty
+print("Resetting to 0")
+time.sleep(1)    
+servo1.ChangeDutyCycle(0)
+
+servo1.stop()
+GPIO.cleanup()
+print("Exiting")
