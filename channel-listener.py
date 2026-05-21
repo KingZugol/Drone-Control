5aa053ba548b9ee68f175c9b9fcb68fdce0e0f82
Lars <test@test.com>
2026-05-21 18:58:29 +0200

    Implementation to read EdgeTX Global Variable on RC channel 11

--- channel-listener.py
+++ channel-listener.py
@@ -15,7 +15,8 @@ message= master.mav.command_long_encode(
 )
 master.mav.send(message)
 response = master.recv_match(type='COMMAND_ACK', blocking=True)
-pwm_val = 0 
+pwm_val = 0
+gvar_pwm= 0 
 if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
     print("Command accepted")
     try:
@@ -25,11 +26,14 @@ if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL
                 if not channel_response:
                     break
                 pwm_val = channel_response.chan8_raw
+                gvar_pwm = channel_response.chan11_raw
             print(f"Channel 8 PWM: {pwm_val}")
+            gvar = round((gvar_pwm -1500) * 0.2)
+            print(f"Channel 11 PWM: {gvar_pwm}, GVAR: {gvar}")
             time.sleep(0.5)
     except KeyboardInterrupt:
         print("Exiting...")
 else:
     print("Command failed")
 
-
+#### Works ####
