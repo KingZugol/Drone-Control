86ee26dead5298a043e34e97ed9832526c21377f
Lars <test@test.com>
2026-05-24 16:12:54 +0200

    crsf parser, listening to 0x0200 subid

---
+++ crsf-listener.py
@@ -0,0 +1,34 @@
+import crc_calc as crc
+import serial
+import struct
+import time
+
+def parse_crsf(port):
+    if port.in_waiting >=10:
+        header = port.read(1)
+        if header == b'\xC8': #Adresse Flightcontroller
+            length = int.from_bytes(port.read(port.read(1), byteorder='big'))
+            packet_body = port.read(length - 1)
+            crc_value = port.read(1)
+
+            if crc_value == bytes([crc.crsf_crc8(packet_body)]):
+                if packet_body == 0x80:
+                    subtype_id = struct.unpack('>H', packet_body[1:3])
+                    payload = packet_body[3:7]
+                    payload_val = struct.unpack('>i', payload)
+                    
+                    return subtype_id, payload_val
+                
+    return None, None
+
+try:
+    port = serial.Serial('/dev/serial0', baudrate=420000, timeout=0.01)
+    sub_id, val = parse_crsf(port)
+    if sub_id == 0x0200:
+        print(f'EdgeTX Val: {val}')
+
+except KeyboardInterrupt:
+    print('exiting')
+finally:
+    if 'port' in locals() and port.is_open:
+        port.close()
