b45f550693932f18f49bc7d0b459dcee03c4a9d8
Lars <test@test.com>
2026-06-09 15:16:51 +0200

    fixing pin numbering

--- crsf-listener.py
+++ crsf-listener.py
@@ -7,8 +7,8 @@ import RPi.GPIO as GPIO
 #GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
 #Pin 18 als output
-GPIO.setup(18,GPIO.OUT)
-servo1=GPIO.PWM(18,50) #Pin 18 out, 50hz
+GPIO.setup(12,GPIO.OUT)
+servo1=GPIO.PWM(12,50) #Pin 18 out, 50hz
 servo1.start(0)
 
 def set_angle(angle, servo):
